bacdf6125a8f186f8407bf88ffa4af30994700ca
mk <pg.studykm@gmail.com>
2021-08-10 02:11:08 +0000

    Procfile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,19 +22,20 @@ class UsersController extends Controller
     
     public function show($id)
     {
-        // idの値でユーザーを検索して取得
+        // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
-        
-         // 関係するモデルの件数をロード
+
+        // 関係するモデルの件数をロード
         $user->loadRelationshipCounts();
 
         // ユーザの投稿一覧を作成日時の降順で取得
         $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        //ユーザー詳細ビューでそれを表示
-        return view('users.show',[
+        // ユーザ詳細ビューでそれらを表示
+        return view('users.show', [
             'user' => $user,
-            ]);
+            'microposts' => $microposts,
+        ]);
         
     }
     
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -1,4 +1,5 @@
-@if (count($microposts) > 0)
+
+@if (count ($microposts) > 0)
     <ul class="list-unstyled">
         @foreach ($microposts as $micropost)
             <li class="media mb-3">
@@ -14,14 +15,12 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div>
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         @endif
-                    </div>
                 </div>
             </li>
         @endforeach
